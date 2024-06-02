de6c0ce0538413edc7952bfa38f79ff294933672
Ritesh-1310 <ranjanritesh13102001@gmail.com>
2024-06-02 14:31:11 +0530

    removed unneccessary comment lines

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,3 @@
-// // Initialization 
-// require("dotenv").config();
-
-// const express = require('express');
-// const app = express();
-
-// const mongoose = require('mongoose');
-// // const Note = require('./models/Note');
-
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
-// mongoose.set('strictQuery', true);
-
-// const mongoDbPath = process.env.mongoDbPath
-
-
-// mongoose.connect(mongoDbPath).then(function() {
-//     app.get("/api", function(req, res) {
-//         const response = { statuscode: res.statusCode, message: "API Works!" };
-//         res.json(response);
-//     });
-    
-//     const noteRouter = require('./routes/Note');
-//     app.use("/notes", noteRouter);
-// });
-
-// // Starting the server on a PORT
-// const PORT = process.env.PORT || 8000;
-// app.listen(PORT, function() {
-//     console.log("Server started at PORT: " + PORT);
-// });
-
-//--------------------------------------------------------------
 require("dotenv").config();
 
 const express = require('express');
@@ -51,7 +16,7 @@ mongoose.connect(mongoDbPath, { useNewUrlParser: true, useUnifiedTopology: true 
     .then(() => {
         console.log("MongoDB connected...");
 
-        app.get("/api", (req, res) => {
+        app.get("/", (req, res) => {
             const response = { statuscode: res.statusCode, message: "API Works!" };
             res.json(response);
         });
@@ -66,4 +31,3 @@ mongoose.connect(mongoDbPath, { useNewUrlParser: true, useUnifiedTopology: true 
     })
     .catch(err => console.log(err));
 
-    
